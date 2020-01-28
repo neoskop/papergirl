84c8b5891197d794ae269dd1bfa05b278abf0c05
Arne Diekmann <arne@neoskop.de>
2020-01-28 18:57:50 +0100

    feat: use yq to change papergirl tag in values too

--- release.sh
+++ release.sh
@@ -2,20 +2,19 @@
 
 set -e
 
-function check_command() {
-  if ! command -v $1 >/dev/null; then
-    echo -e "Install \033[1m$1\033[0m"
-    exit 1
-  fi
+function check_commands() {
+  for command in $@ ; do
+    if ! command -v $command >/dev/null; then
+      echo -e "Install \033[1m$command\033[0m"
+      exit 1
+    fi
+  done
 }
 
-check_command git
-check_command yarn
-check_command npm
-check_command jq
+check_commands git yarn npm jq yq
 
 if [[ "$#" != "1" ]] || [[ ! "$1" =~ ^(patch|minor|major)$ ]]; then
-  echo "Usage: $0 patch|minor|major"
+  echo -e "Usage: $0 \033[1mpatch|minor|major\033[0m"
   exit 1
 fi
 
@@ -31,6 +30,9 @@ npm version --no-git-tag-version $1
 version=`cat package.json | jq -r .version`
 sed -i "s/appVersion: .*/appVersion: \"$version\"/" helm/Chart.yaml
 sed -i "s/version: .*/version: $version/" helm/Chart.yaml
+yq w -i helm/Chart.yaml version $version
+yq w -i helm/Chart.yaml appVersion $version
+yq w helm/values.yaml papergirl.image.tag $version
 git add .
 git commit -m "chore: Bump version to ${version}."
 git push
