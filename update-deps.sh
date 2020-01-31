fe549696c238c3c6dc757461e59832bfe04c83c4
Arne Diekmann <arne@neoskop.de>
2020-01-31 18:08:57 +0100

    feat: init buckets on startup

--- update-deps.sh
+++ update-deps.sh
@@ -26,7 +26,7 @@ warn() {
 }
 
 check_commands() {
-  for command in $@ ; do
+  for command in $@; do
     if ! command -v $command >/dev/null; then
       echo -e "Install $(bold $command)"
       exit 1
@@ -35,22 +35,24 @@ check_commands() {
 }
 
 get_tags() {
-    curl -s https://hub.docker.com/v2/repositories/$1/tags/?page_size=10000 | jq -r '.results | map(.name) | .[]'
+  curl -s https://hub.docker.com/v2/repositories/$1/tags/?page_size=10000 | jq -r '.results | map(.name) | .[]'
 }
 
 check_commands yarn-check yarn jq yq
 yarn-check -u
 echo "Will use the following new image versions:"
-NGINX_LATEST_TAG=`get_tags library/nginx | grep '^[0-9]*\.[0-9]*\.[0-9]*$' | sort -V | tail -n 1`
+NGINX_LATEST_TAG=$(get_tags library/nginx | grep '^[0-9]*\.[0-9]*\.[0-9]*$' | sort -V | tail -n 1)
 echo "  - NGINX: $(bold $NGINX_LATEST_TAG)"
-NGINX_PROMETHEUS_EXPORTER_LATEST_TAG=`get_tags nginx/nginx-prometheus-exporter | grep '^[0-9]*\.[0-9]*\.[0-9]*$' | sort -V | tail -n 1`
+NGINX_PROMETHEUS_EXPORTER_LATEST_TAG=$(get_tags nginx/nginx-prometheus-exporter | grep '^[0-9]*\.[0-9]*\.[0-9]*$' | sort -V | tail -n 1)
 echo "  - NGINX Prometheus Exporter: $(bold $NGINX_PROMETHEUS_EXPORTER_LATEST_TAG)"
-NATS_LATEST_TAG=`get_tags library/nats | grep '^[0-9]*\.[0-9]*\.[0-9]*-scratch$' | sort -V | tail -n 1`
+NATS_LATEST_TAG=$(get_tags library/nats | grep '^[0-9]*\.[0-9]*\.[0-9]*-scratch$' | sort -V | tail -n 1)
 echo "  - NATS: $(bold $NATS_LATEST_TAG)"
-NATS_LATEST_VERSION=`get_tags library/nats | grep '^[0-9]*\.[0-9]*\.[0-9]*$' | sort -V | tail -n 1`
-MINIO_LATEST_TAG=`get_tags minio/minio | grep '^RELEASE' | sort | tail -n 1`
+NATS_LATEST_VERSION=$(get_tags library/nats | grep '^[0-9]*\.[0-9]*\.[0-9]*$' | sort -V | tail -n 1)
+MINIO_LATEST_TAG=$(get_tags minio/minio | grep '^RELEASE' | sort | tail -n 1)
 echo "  - MinIO: $(bold $MINIO_LATEST_TAG)"
-BUSYBOX_LATEST_TAG=`get_tags library/busybox | grep '^[0-9]*\.[0-9]*\.[0-9]*$' | sort -V | tail -n 1`
+MINIO_MC_LATEST_TAG=$(get_tags minio/mc | grep '^RELEASE' | sort | tail -n 1)
+echo "  - MinIO CLI: $(bold $MINIO_MC_LATEST_TAG)"
+BUSYBOX_LATEST_TAG=$(get_tags library/busybox | grep '^[0-9]*\.[0-9]*\.[0-9]*$' | sort -V | tail -n 1)
 echo "  - Busybox: $(bold $BUSYBOX_LATEST_TAG)"
 yq w -i docker-compose.yml services.webserver.image nginx:$NGINX_LATEST_TAG
 yq w -i docker-compose.yml services.queue.image nats:$NATS_LATEST_TAG
@@ -58,5 +60,6 @@ yq w -i docker-compose.yml services.s3.image minio/minio:$MINIO_LATEST_TAG
 yq w -i helm/values.yaml nginx.image.tag $NGINX_LATEST_TAG
 yq w -i helm/values.yaml prometheus.nginxExporterImage.tag $NGINX_PROMETHEUS_EXPORTER_LATEST_TAG
 yq w -i helm/values.yaml minio.image.tag $MINIO_LATEST_TAG
+yq w -i helm/values.yaml bucketSetup.image.tag $MINIO_MC_LATEST_TAG
 yq w -i helm/values.yaml nats.version $NATS_LATEST_VERSION
 yq w -i helm/values.yaml volumeSetup.image.tag $BUSYBOX_LATEST_TAG
