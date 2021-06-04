ec38cfb9a503068e9b3e23be75a5e7a0a4da7022
Arne Diekmann <arne@neoskop.de>
2021-06-04 13:36:48 +0200

    feat: build and use local image in kind

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -2,7 +2,37 @@
 set -e
 
 if ! kind get clusters | grep -o papergirl &>/dev/null ; then
-    kind create cluster --name papergirl
+    reg_name='kind-registry'
+    reg_port='5000'
+    running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+    if [ "${running}" != 'true' ]; then
+        docker run \
+            -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+            registry:2
+    fi
+
+cat <<EOF | kind create cluster --name papergirl --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
+    endpoint = ["http://${reg_name}:${reg_port}"]
+EOF
+
+    docker network connect "kind" "${reg_name}" || true
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
 fi
 
 kubectl config use-context kind-papergirl &>/dev/null
@@ -32,11 +62,16 @@ while ! kubectl get crd tenants.minio.min.io &>/dev/null ; do
     sleep 1
 done
 
+docker build -t localhost:5000/papergirl:latest .
+docker push localhost:5000/papergirl:latest
+
 if ! helm status papergirl -n papergirl &>/dev/null ; then
     kubectl create ns papergirl
     helm install papergirl -n papergirl -f quickstart-values.yaml ./helm
 else
     helm upgrade papergirl -n papergirl -f quickstart-values.yaml ./helm
+    kubectl rollout restart sts/papergirl
+    kubectl rollout restart sts/papergirl-preview
 fi
 
 kubectl config set-context --current --namespace=papergirl &>/dev/null
