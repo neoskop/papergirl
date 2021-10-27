188dccfc2c68322d41bec6c9ba4401ee5d023758
Arne Diekmann <arne@neoskop.de>
2021-10-27 12:45:11 +0200

    feat: for backwards compatibility delete existing root.conf files

---
+++ src/update/nginx/listeners/legacy-root-config.listener.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '../../../config/config.service';
+import { Meta } from '../../../meta/meta.interface';
+import { ConfigListener } from './config.listener';
+
+@Injectable()
+export class LegacyRootConfigListener extends ConfigListener {
+  constructor(config: ConfigService) {
+    super(config, 'root.conf');
+  }
+
+  protected getConfigLines(_: Meta): string[] {
+    return [];
+  }
+
+  protected shouldCreateConfigFile(_: Meta): boolean {
+    return false;
+  }
+}
